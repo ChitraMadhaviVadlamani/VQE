9b3ad06cfc8be7bef59dc383dc1ff97523b120af
ChitraMadhaviVadlamani <76861203+ChitraMadhaviVadlamani@users.noreply.github.com>
2022-03-01 16:14:42 +0530

    TSP brute force approach code

---
+++ VQE/TSP_BF.py
@@ -0,0 +1,82 @@
+# useful additional packages
+import matplotlib.pyplot as plt
+import matplotlib.axes as axes
+import numpy as np
+import networkx as nx
+
+from qiskit import Aer
+from qiskit.tools.visualization import plot_histogram
+from qiskit.circuit.library import TwoLocal
+from qiskit_optimization.applications import Maxcut, Tsp
+from qiskit.algorithms import VQE, NumPyMinimumEigensolver
+from qiskit.algorithms.optimizers import SPSA
+from qiskit.utils import algorithm_globals, QuantumInstance
+from qiskit_optimization.algorithms import MinimumEigenOptimizer
+from qiskit_optimization.problems import QuadraticProgram
+
+def draw_graph(G, colors, pos):
+    default_axes = plt.axes(frameon=True)
+    nx.draw_networkx(G, node_color=colors, node_size=600, alpha=0.8, ax=default_axes, pos=pos)
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
+###########################
+n = 3
+num_qubits = n ** 2
+tsp = Tsp.create_random_instance(n, seed=123)
+adj_matrix = nx.to_numpy_matrix(tsp.graph)
+print("distance\n", adj_matrix)
+
+colors = ["r" for node in tsp.graph.nodes]
+pos = [tsp.graph.nodes[node]["pos"] for node in tsp.graph.nodes]
+draw_graph(tsp.graph, colors, pos)
+#####################################
+
+
+from itertools import permutations
+
+
+def brute_force_tsp(w, N):
+    a = list(permutations(range(1, N)))
+    last_best_distance = 1e10
+    for i in a:
+        distance = 0
+        pre_j = 0
+        for j in i:
+            distance = distance + w[j, pre_j]
+            pre_j = j
+        distance = distance + w[pre_j, 0]
+        order = (0,) + i
+        if distance < last_best_distance:
+            best_order = order
+            last_best_distance = distance
+            print("order = " + str(order) + " Distance = " + str(distance))
+    return last_best_distance, best_order
+
+
+best_distance, best_order = brute_force_tsp(adj_matrix, n)
+print(
+    "Best order from brute force = "
+    + str(best_order)
+    + " with total distance = "
+    + str(best_distance)
+)
+###################
+
+def draw_tsp_solution(G, order, colors, pos):
+    G2 = nx.DiGraph()
+    G2.add_nodes_from(G)
+    n = len(order)
+    for i in range(n):
+        j = (i + 1) % n
+        G2.add_edge(order[i], order[j], weight=G[order[i]][order[j]]["weight"])
+    default_axes = plt.axes(frameon=True)
+    nx.draw_networkx(
+        G2, node_color=colors, edge_color="b", node_size=600, alpha=0.8, ax=default_axes, pos=pos
+    )
+    edge_labels = nx.get_edge_attributes(G2, "weight")
+    nx.draw_networkx_edge_labels(G2, pos, font_color="b", edge_labels=edge_labels)
+
+
+draw_tsp_solution(tsp.graph, best_order, colors, pos)
+
+#########################
